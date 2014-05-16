362b7257fe53eda11d022e59c0f962073da700a5
ashawhat <ashlcycle@gmail.com>
2014-05-16 12:05:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This widget accesses upcoming scheduled posts.  Client can choose amount to query, if post includes a summary, and date format.  Customizable CSS
---
+++ coming-next.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Hoverboard Analytics
+Version: 1.0.0
+Plugin URI: https://github.com/copterlabs/hoverboard-analytics
+Description: Adds Google Analytics integration to Hoverboard
+Author: Copter Labs
+Author URI: http://copterlabs.com/
+License: MIT
+*/
